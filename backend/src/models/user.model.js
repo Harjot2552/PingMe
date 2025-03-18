5af448fbb9eb41e4fb5b18cb65f370756c4197cb
Harjot <hajotsingh2552@gmail.com>
2025-03-18 17:51:20 -0600

    exported the userSchema using mongoose,

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -14,7 +14,17 @@ const userSchema = new mongoose.Schema(
         password:{
             type: String,
             required: true,
+            minlength: 6,
         },
-    }
+        profilePic:{
+            type: String,
+            default: "",
+        }
+    },
+    {timestamps: true}
 
 );
+
+const User = mongoose.model("User", userSchema); 
+
+export default User;
