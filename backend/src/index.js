c548681d2862082ef32bed7b2a3fc115c223834e
Harjot <hajotsingh2552@gmail.com>
2025-03-18 17:23:11 -0600

    Created the .env file and using the information from their with the help of dotenv package.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
 
 const app = express();
+dotenv.config();
 
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes)
 
-app.listen(5002, ()=>{
+app.listen(PORT, ()=>{
     console.log("Server is running!!")
 })
