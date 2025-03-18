5592cf01989fb45d6459824cea17d781e2812911
Harjot <hajotsingh2552@gmail.com>
2025-03-18 17:13:25 -0600

    imported the sign up, login and logout function in the routes file

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller";
 
 const router = express.Router();
 
-router.post("/signup", )
-router.post("/login",)
-router.get("/logout", )
+router.post("/signup", signup )
+router.post("/login", login)
+router.get("/logout", logout )
 
 export default router;
