f2dd287df3791e5e34bf0fce0b50975f0b803cdc
Harjot <hajotsingh2552@gmail.com>
2025-03-18 19:30:10 -0600

    Checking if the user exist or not.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -47,7 +47,11 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     const {email, password} = req.body;
     try {
-      const user = await User.findOne({email})
+      const user = await User.findOne({email});
+
+      if(!user){
+       return res.status(400).json({message: "Invalid credentials"})
+      }
     } catch (error) {
       
     }
