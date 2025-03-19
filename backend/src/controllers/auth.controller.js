4949bb8ccc895b64721b31977e666e80fa9d11a8
Harjot <hajotsingh2552@gmail.com>
2025-03-18 18:04:27 -0600

    Checking if the email is already exist

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,7 +6,10 @@ export const signup = async (req, res) => {
     if(password.length > 6){
       return res.status(400).json({message: "Password must be at least 6 characters"})
     }
-    const user = await User.findOne()
+    const user = await User.findOne({email})
+    if (user) return res.status(400).json({message: "Email already exist"})
+
+
   } catch (error) {
     
   }
