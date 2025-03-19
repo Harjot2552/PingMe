e73657cd4302bd4e59e65a79b382fd530db7d093
Harjot <hajotsingh2552@gmail.com>
2025-03-18 18:21:23 -0600

    sending the token to the cookie

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,5 +1,10 @@
 import jwt from "jsonwebtoken"
 
 export const generateToken = (userId, res) =>{
-    const token = jwt.sign({userId}, process.env.JWT_SECRET)
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn:"7d"
+        
+    })
+
+    res.cookie("jwt", token)
 }
