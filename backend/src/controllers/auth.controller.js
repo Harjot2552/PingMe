9f7823f1cea157f5b951bb165c601322045fe5bd
Harjot <hajotsingh2552@gmail.com>
2025-03-18 18:53:45 -0600

    Checking if the fields are empty setting the status to 400 and not saving it into database

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,7 +5,10 @@ import bcrypt from "bcryptjs"
 export const signup = async (req, res) => {
   const {fullName, email, password} = req.body
   try {
-    if(password.length > 6){
+    if(!fullName || !email || !password){
+      return res.status(400).json({message: "All fields are required."})
+    }
+    if(password.length < 6){
       return res.status(400).json({message: "Password must be at least 6 characters"})
     }
     const user = await User.findOne({email})
@@ -15,8 +18,8 @@ export const signup = async (req, res) => {
       const hashedPassword = await bcrypt.hash(password, salt);
 
       const newUser = new User({
-        fullName: fullName,
-        email: email,
+        fullName,
+        email,
         password: hashedPassword,
       })
 
@@ -24,11 +27,20 @@ export const signup = async (req, res) => {
         // generate the jwt token here
         generateToken(newUser._id, res)
         await newUser.save();
+
+        res.status(201).json({
+          _id:newUser._id,
+          fullName: newUser.fullName,
+          email: newUser.email,
+          profilePic: newUser.profilePic,
+        })
+
       }else{
         res.status(400).json({message: "Invalid user data"})
       }
   } catch (error) {
-    
+    console.log("Error in siging up", error.message);
+    res.status(500).json({message: "Internal server error"})
   }
 };
 
