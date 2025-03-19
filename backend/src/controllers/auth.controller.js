f2c3f661f3d19cbd4141e4df7a6af4578ad6febf
Harjot <hajotsingh2552@gmail.com>
2025-03-18 19:45:12 -0600

    Catching the error in console and setting the response status to 500 for internal server error.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -72,7 +72,14 @@ export const login = async (req, res) => {
   };
 
   export const logout = (req, res) => {
-    res.cookie("jwt", "", {
-      maxAge: 0,
-    })
+    try {
+      res.cookie("jwt", "", {
+        maxAge: 0,
+      })
+      return res.status(200).json({message: "Logged out successfully"})
+    } catch (error) {
+      console.log("Error in login controller", error.message)
+      return res.status(500).json({message: "Internal Server Error"})
+    }
+
   };
