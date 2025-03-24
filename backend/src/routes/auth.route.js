937c52d00b9a7a4c8050f7144adc6b5b8f4f4a5a
Harjot <hajotsingh2552@gmail.com>
2025-03-24 17:45:42 -0600

    Created therput method for the update profile router

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/signup", signup )
 router.post("/login", login)
-router.get("/logout", logout )
+router.post("/logout", logout )
+router.put("/update-profile", updateProfile)
 
 export default router;
