9339498a5bc30631c7ee48dad1fd10d92a7b7088
Harjot <hajotsingh2552@gmail.com>
2025-03-24 18:44:29 -0600

    Created the checkAuth route to checking if the user is authenticated or not

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -100,3 +100,12 @@ export const updateProfile = async (req, res) =>{
     res.status(500).json({message: "Internal Server Error"})
   }
 }
+
+export const checkAuth = async(req, res) =>{
+  try {
+    res.status(200).json(req.user)
+  } catch (error) {
+    console.log("Internal server error")
+    res.status(500).json({message: "Internal Server Error"})
+  }
+}
