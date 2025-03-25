3e7fe1c1b0b55a4a93182d5548973a4ca09264c4
Harjot <hajotsingh2552@gmail.com>
2025-03-24 18:31:02 -0600

    if user have not provided the profile pic then sending the 400 status otherwise uploading it to the cloud using cloudinary

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -87,6 +88,11 @@ export const updateProfile = async (req, res) =>{
   try {
     const {profilePic} = req.body;
     const userId = req.user._id
+
+    if(!profilePic){
+      return res.status(400).json({message: "Profile pic is required"})
+    }
+    await cloudinary.uploader.upload(profilePic)
   } catch (error) {
     
   }
