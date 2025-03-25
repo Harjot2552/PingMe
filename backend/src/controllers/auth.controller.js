bc2525fc989a6987c193c90e94b01ceb3b045e20
Harjot <hajotsingh2552@gmail.com>
2025-03-24 18:27:49 -0600

    Grabbing the userId  in the updateProfile function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -84,4 +84,10 @@ export const logout = (req, res) => {
 
 export const updateProfile = async (req, res) =>{
 
+  try {
+    const {profilePic} = req.body;
+    const userId = req.user._id
+  } catch (error) {
+    
+  }
 }
