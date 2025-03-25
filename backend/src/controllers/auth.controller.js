cd25c01e5adf443080745c252ca4c82e2858e16f
Harjot <hajotsingh2552@gmail.com>
2025-03-24 18:38:22 -0600

    Sending the 200 status with the updatedUser function and sending the 500 status if anything didn't work

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -93,8 +93,10 @@ export const updateProfile = async (req, res) =>{
       return res.status(400).json({message: "Profile pic is required"})
     }
    const uploadResponse = await cloudinary.uploader.upload(profilePic)
-   const updatedUser = await  User.findOneAndUpdate(userId, {profilePic: uploadResponse.secure_url})
+   const updatedUser = await  User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true})
+   res.status(200).json(updatedUser)
   } catch (error) {
-    
+    console.log("Error in updating in profile")
+    res.status(500).json({message: "Internal Server Error"})
   }
 }
