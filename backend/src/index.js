1dacc15f128827d421df51fb65ebbaf53ae681b0
Harjot <hajotsingh2552@gmail.com>
2025-03-31 16:46:27 -0600

    Created the messaeg route file and imported in the index file, also created the new endpoint for the message

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoute from "./routes/message.route.js"
 import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js"
 import cookieParser from 'cookie-parser'
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoute)
 
 app.listen(PORT, ()=>{
     console.log("Server is running!!")
