c932967b0d36dd2c9a43ed1ba4a5d3cce732b2ee
Harjot <hajotsingh2552@gmail.com>
2025-03-31 17:09:21 -0600

    Created the get route for the user and make it protected so that authenticated user can online user

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,9 @@
 import express from "express"
+import { protectRoute } from "../middleware/auth.middleware";
 
 const router = express.Router();
+
+router.get("/users", protectRoute)
+
 
 export default router;
