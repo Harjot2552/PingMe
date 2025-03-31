487dd62d5d2d6b206a25d1218ffb58a2175b7332
Harjot <hajotsingh2552@gmail.com>
2025-03-31 17:31:08 -0600

    Created the getMessage method and imported it in the message.route

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -11,3 +11,11 @@ export const getUsersForSidebar = async(req, res) =>{
         res.status(500).json({error: "Internal Server Error"})
     }
 }
+
+export const getMessages = async(req, res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
