e2b9ac8f0de700361f330f78ea8cb4a79985630e
Harjot <hajotsingh2552@gmail.com>
2025-04-03 18:50:27 -0600

    Testing the daisy UI component if they are working or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,14 @@ const App = () => {
   return (
     <div>
       <h1 className='text-red-700 '>Hello World</h1>
+      <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
     </div>
   )
 }
