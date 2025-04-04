e9f3d4f65986ef607fb22c5b2a5aa8557ae089c5
Harjot <hajotsingh2552@gmail.com>
2025-04-04 09:32:03 -0600

    In the authStore file, sending the data to the database using the axios and then showing the success toast

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance  } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set)=>  ({
  authUser: null,
@@ -21,6 +22,12 @@ export const useAuthStore = create((set)=>  ({
  },
 
  signup: async(data)=>{
-    
+    set({isSigningUp: true})
+    try {
+      const res =   await axiosInstance.post("/auth/signup", data);
+      toast.success("Account created successfully")
+    } catch (error) {
+        
+    }
  }
 }))
