4ddef5f59ee675c036c9485e188820da5b26c9fd
Harjot <hajotsingh2552@gmail.com>
2025-04-04 09:45:22 -0600

    created the header with  the light bg and made it sticky to the top

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,9 +4,15 @@ import { useAuthStore } from '../store/useAuthStore'
 const Navbar = () => {
   const {authUser, logout} = useAuthStore();
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <header
+    className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+  backdrop-blur-lg bg-base-100/80"
+  >
+
+
+
+    
+  </header>
   )
 }
 
