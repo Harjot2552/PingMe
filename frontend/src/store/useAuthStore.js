1ab360400c4c9e9a29fbb697c2fb71a55d846292
Harjot <hajotsingh2552@gmail.com>
2025-04-03 19:38:58 -0600

    create the fucntion to check if the user is authenticated or not using the api endpoint we made

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,8 +1,21 @@
 import { create } from "zustand";
+import { axiosInstant } from "../lib/axios";
 
 export const useAuthStore = create((set)=>  ({
  authUser: null,
  isSigningUp: false,
  isLoggingIn: false,
+ isUpdatingProfile: false,
  isCheckingAuth: true,
+
+ checkAuth: async() =>{
+    try {
+        const res =  await axiosInstant.get("/auth/check")
+        set({authUser:res.data})
+    } catch (error) {
+        set({authUser:null})
+    }finally{
+        set({isCheckingAuth: false})
+    }
+ }
 }))
