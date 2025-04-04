2909cbd9d0fbd08cf76a96d9ca69841b0a5b37c7
Harjot <hajotsingh2552@gmail.com>
2025-04-04 09:46:47 -0600

    seprated the navbar into two parts using the flexbox

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { useAuthStore } from '../store/useAuthStore'
+import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Navbar = () => {
-  const {authUser, logout} = useAuthStore();
+  const { authUser, logout } = useAuthStore();
   return (
     <header
-    className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+      className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
   backdrop-blur-lg bg-base-100/80"
-  >
+    >
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8"></div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-
-
-    
-  </header>
-  )
-}
-
-export default Navbar
+export default Navbar;
