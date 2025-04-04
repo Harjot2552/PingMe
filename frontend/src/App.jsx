b855149e3440d5f6533fe96115ae4cc750f9f365
Harjot <hajotsingh2552@gmail.com>
2025-04-03 19:01:08 -0600

    Using the react router created the routes for the different pages like signup, login, logout and settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/signup' element={<SignUpPage/>}></Route>
+        <Route path='/login' element={<LoginPage/>}></Route>
+        <Route path='/settings' element={<SettingsPage/>}></Route>
+        <Route path='/profile' element={<ProfilePage/>}></Route>
+      </Routes>
     </div>
   )
 }
