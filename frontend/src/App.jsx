7648138060c7052f696078b2fcddb623b1118bd6
Harjot <hajotsingh2552@gmail.com>
2025-04-03 18:56:48 -0600

    Deleted the given assests folder and created the two new folder one is component and other one is pages,
    - also created the Navbar component and imported it into app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-red-700 '>Hello World</h1>
-      <button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-info">Info</button>
-<button className="btn btn-success">Success</button>
-<button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button>
+      <Navbar></Navbar>
     </div>
   )
 }
