0e11e4bf2381ce8556f27c14a96df31e523f0df0
Harjot <hajotsingh2552@gmail.com>
2025-04-04 09:54:16 -0600

    Creating the right side of the navbar, firts created the settings link witha icon because all user can access the settings page even they are logged in or not

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,14 +11,33 @@ const Navbar = () => {
   backdrop-blur-lg bg-base-100/80"
     >
       <div className="container mx-auto px-4 h-16">
+        {/* /flexbox */}
         <div className="flex items-center justify-between h-full">
-          <div className="flex items-center gap-8"></div>
-          <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+          {/* logo   */}
+          <div className="flex items-center gap-8">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <Send className="w-5 h-5 text-primary" />
               </div>
               <h1 className="text-lg font-bold">PingMe</h1>
             </Link>
+          </div>
+          {/* rightside */}
+          <div className="flex items-center gap-2">
+          <Link
+              to={"/settings"}
+              className={`
+              btn btn-sm gap-2 transition-colors
+              
+              `}
+            >
+              <Settings className="w-4 h-4" />
+              <span className="hidden sm:inline">Settings</span>
+            </Link>
+          </div>
         </div>
       </div>
     </header>
