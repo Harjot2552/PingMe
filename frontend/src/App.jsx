78852452ea32950f25b3aaf795ca78e33b52b389
Harjot <hajotsingh2552@gmail.com>
2025-04-03 21:39:32 -0600

    now using the authUser on the homepage,  and profile page, if the user is  authenticated then we are letting the user to go to home page otherwise sending the user to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SignUpPage from './pages/SignUpPage'
 import LoginPage from './pages/LoginPage'
@@ -27,11 +27,11 @@ const App = () => {
     <div>
       <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/' element={ authUser ? <HomePage/> : <Navigate to="/login"/>}></Route>
         <Route path='/signup' element={<SignUpPage/>}></Route>
         <Route path='/login' element={<LoginPage/>}></Route>
         <Route path='/settings' element={<SettingsPage/>}></Route>
-        <Route path='/profile' element={<ProfilePage/>}></Route>
+        <Route path='/profile' element={ authUser ? <ProfilePage/> : <Navigate to="/login"/> }></Route>
       </Routes>
     </div>
   )
