f02372b1f65ad9de63823fe7e621831d01c103da
Harjot <hajotsingh2552@gmail.com>
2025-04-04 09:49:00 -0600

    Created the logo using the lucide react icon and using the link from the react router dom made that logo clickable which will take user to the homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import { Send } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { authUser, logout } = useAuthStore();
@@ -11,6 +13,12 @@ const Navbar = () => {
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8"></div>
+          <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <Send className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">PingMe</h1>
+            </Link>
         </div>
       </div>
     </header>
