e6fa893c072e15755d179c14ac458c0255f298b4
Harjot <hajotsingh2552@gmail.com>
2025-04-04 10:07:13 -0600

    Imported some same icons that we used in signup page and Link from the react router dom

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import { Eye, EyeOff, Loader2, Mail, Send } from "lucide-react";
+import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -26,7 +29,7 @@ const LoginPage = () => {
               className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20
             transition-colors"
             >
-              <MessageSquare className="w-6 h-6 text-primary" />
+              <Send className="w-6 h-6 text-primary" />
             </div>
             <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
             <p className="text-base-content/60">Sign in to your account</p>
