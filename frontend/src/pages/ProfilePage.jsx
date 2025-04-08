a7b71db8380fe17d59783bc0b43549f89daaa491
Harjot <hajotsingh2552@gmail.com>
2025-04-08 17:08:05 -0600

    created the empty function to handle the image

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 
 const ProfilePage = () => {
-  const {authuser, isUpdatingProfile, updateProfile}  = useAuthStore()
+  const {authuser, isUpdatingProfile, updateProfile}  = useAuthStore();
+  const handleUploadImage = async (e) =>{
+
+  }
   return (
     <div>
       
