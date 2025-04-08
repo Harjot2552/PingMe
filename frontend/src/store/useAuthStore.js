eb057cc848cbc91092565a28a94e5a792aff517f
Harjot <hajotsingh2552@gmail.com>
2025-04-08 17:43:03 -0600

    showing the succes and error toast according to the user action

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -60,8 +60,11 @@ export const useAuthStore = create((set) => ({
 
     try {
       const res  = await axiosInstance.put("/auth/update-profile", data)
+      set({authUser: res.data});
+      toast.success("Profile Updated Successfully")
     } catch (error) {
-      
+      console.log("error in update profile", error);
+      toast.error(error.response.data.message)
     }finally{
       set({isUpdatingProfile: false});
     }
