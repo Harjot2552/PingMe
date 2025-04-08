79d99ea9bc35a1f6dbc73af5c2845d77cecf6a30
Harjot <hajotsingh2552@gmail.com>
2025-04-08 17:12:26 -0600

    Created the two more divs and added some tailwind css properties

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -8,7 +8,12 @@ const ProfilePage = () => {
   }
   return (
     <div className='h-screen pt-20'>
-      
+      <div className='max-w-2xl mx-auto p-4 py-8'>
+        <div className='bg-base-300 rounded-xl px-6 space-y-8'>
+
+        </div>
+
+      </div>
     </div>
   )
 }
