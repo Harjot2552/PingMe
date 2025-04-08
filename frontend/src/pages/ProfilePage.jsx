41be3acb3b655a1ff22af81a894cfd6d924db97a
Harjot <hajotsingh2552@gmail.com>
2025-04-08 17:15:13 -0600

    Added H1 and paragraph for the profile heading

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -10,7 +10,11 @@ const ProfilePage = () => {
     <div className='h-screen pt-20'>
       <div className='max-w-2xl mx-auto p-4 py-8'>
         <div className='bg-base-300 rounded-xl px-6 space-y-8'>
+          <div className='text-center'>
+            <h1 className='text-2xl font-semibold'>Profile</h1>
+            <p className='mt-2'>Your profile information</p>
 
+          </div>
         </div>
 
       </div>
