7dac0b16ff3ce70f8e9a2a8e2553e13f577767a4
Harjot <hajotsingh2552@gmail.com>
2025-04-09 16:30:12 -0600

    getting the messages of the specific user where user selected

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useChatStore} from "../store/useChatStore "
 
 const ChatContainer = () => {
   const {isMessagesLoading, messages, getMessages, selectedUser} = useChatStore();
+
+useEffect(()=>{
+  getMessages(selectedUser._id)
+}, [selectedUser._id, getMessages])
 
   if(isMessagesLoading) return <div>Loading..</div>
   return (
