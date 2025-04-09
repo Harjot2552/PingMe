9cb24d8244d95c150e01aa19a6c2d62213bb6144
Harjot <hajotsingh2552@gmail.com>
2025-04-08 19:28:51 -0600

    created the two divs made the second div flexbox

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,8 +4,10 @@ import { useChatStore } from '../store/useChatStore'
 const HomePage = () => {
   const {selectedUser} = useChatStore();
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='h-screen bg-base-200'>
+      <div className='flex items-center justify-center pt-20 px-4'>
+
+      </div>
     </div>
   )
 }
