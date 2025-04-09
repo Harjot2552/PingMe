c2f005d8664fb5871009efec525ecc0b87502cdd
Harjot <hajotsingh2552@gmail.com>
2025-04-09 16:37:28 -0600

    Imported the onlineUsers and selected users from the chatStore and AuthStore

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const ChatHeader = () => {
+    const { selectedUser, setSelectedUser } = useChatStore();
+    const { onlineUsers } = useAuthStore();
   return (
     <div>
       
