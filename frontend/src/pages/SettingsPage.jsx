f2fa0e9fc74139f37122b3d7dd1274ec2d7febae
Harjot <hajotsingh2552@gmail.com>
2025-04-08 18:21:34 -0600

    Imported the theme store in the settings page

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useThemeStore } from '../store/useThemeStore'
+const PREVIEW_MESSAGES = [
+  {id: 1, content: "Hey! How's it's going?", isSent: false},
+  {id: 2, content: "I'm doing great! Just working on some new feature.", isSent: false},
+]
 
 const SettingsPage = () => {
+  const {theme, setTheme} = useThemeStore();
   return (
     <div>
       
