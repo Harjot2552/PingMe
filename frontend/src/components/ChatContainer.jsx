457bddc6f10bc9520204576c7e554da776f5c08b
Harjot <hajotsingh2552@gmail.com>
2025-04-09 16:28:05 -0600

    Added the if statement if the messages are loading then showing the loading component

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import {useChatStore} from "../store/useChatStore "
 
 const ChatContainer = () => {
-  const {isMessagesLoading, messages, getMessages} = useChatStore();
+  const {isMessagesLoading, messages, getMessages, selectedUser} = useChatStore();
+
+  if(isMessagesLoading) return <div>Loading..</div>
   return (
     <div>
       
