73b3fa935323cc37d51f99268162931316c71a17
Harjot <hajotsingh2552@gmail.com>
2025-04-09 16:46:19 -0600

    Creating the loading state more beatiful by making the skelton of the messages

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,23 +1,32 @@
-import React, { useEffect } from 'react'
-import {useChatStore} from "../store/useChatStore "
-import ChatHeader from './ChatHeader';
-import MessageInput from './MessageInput';
+import React, { useEffect } from "react";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import { useChatStore } from "../store/useChatStore";
+import MessageSkelton from "./skeltons/MessageSkelton"
 
 const ChatContainer = () => {
-  const {isMessagesLoading, messages, getMessages, selectedUser} = useChatStore();
+  const { isMessagesLoading, messages, getMessages, selectedUser } =
+    useChatStore();
 
-useEffect(()=>{
-  getMessages(selectedUser._id)
-}, [selectedUser._id, getMessages])
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
 
-  if(isMessagesLoading) return <div>Loading..</div>
+  if (isMessagesLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkelton />
+        <MessageInput />
+      </div>
+    );
   return (
-    <div className='flex-1 flex flex-col overflow-auto'>
-      <ChatHeader/>
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
 
-      <MessageInput/>
+      <MessageInput />
     </div>
-  )
-}
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
