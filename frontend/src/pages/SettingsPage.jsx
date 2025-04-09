273050d8cab3115a12de8fc57757df501ffe6b87
Harjot <hajotsingh2552@gmail.com>
2025-04-08 18:23:46 -0600

    created the div and gave it full screen width

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useThemeStore } from '../store/useThemeStore'
+
+
 const PREVIEW_MESSAGES = [
   {id: 1, content: "Hey! How's it's going?", isSent: false},
   {id: 2, content: "I'm doing great! Just working on some new feature.", isSent: false},
@@ -8,7 +10,8 @@ const PREVIEW_MESSAGES = [
 const SettingsPage = () => {
   const {theme, setTheme} = useThemeStore();
   return (
-    <div>
+    <div className='h-screen container mx-auto px-4 pt-20 max-w-5xl'>
+      
       
     </div>
   )
