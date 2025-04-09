4313840ccf9759e9cdccc29dcf6695dcd9b1a644
Harjot <hajotsingh2552@gmail.com>
2025-04-08 19:16:51 -0600

    Creating the states, seeting there initial values

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -3,5 +3,13 @@ import toast from "react-hot-toast"
 import { axiosInstance } from "../lib/axios";
 
 export const useChatStore = create((set) => ({
-    
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+    getUsers: async () =>{
+        
+    }
 }))
