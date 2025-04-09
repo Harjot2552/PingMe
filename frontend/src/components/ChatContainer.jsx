fac07bf287972d9a803635f1c878c43533185d87
Harjot <hajotsingh2552@gmail.com>
2025-04-09 16:35:18 -0600

    Created the component files respectively and imported them in the chatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import {useChatStore} from "../store/useChatStore "
+import ChatHeader from './ChatHeader';
+import MessageInput from './MessageInput';
 
 const ChatContainer = () => {
   const {isMessagesLoading, messages, getMessages, selectedUser} = useChatStore();
