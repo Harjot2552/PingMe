0c25af1613a445a68144d50c6113fbfa70a30fdd
Harjot <hajotsingh2552@gmail.com>
2025-04-09 17:03:46 -0600

    Created the three empty functions for the imageChange, removeImage and sendMessage

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,9 +1,23 @@
 import React, { useRef, useState } from 'react'
+import { useChatStore } from '../store/useChatStore';
 
 const MessageInput = () => {
     const {text, setText} = useState("")
     const {imgPreview, setImgPreview} = useState(null);
     const fileInputRef = useRef(null);
+    const {sendMessages} = useChatStore();
+
+    const handleImageChange = (e) =>{
+
+    }
+
+    const removeImage = () =>{
+
+    }
+
+    const handleSendMessage = async (e) =>{
+        
+    }
 
   return (
     <div>
