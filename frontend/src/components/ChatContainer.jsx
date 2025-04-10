00f63aa6ec81911cfe60a07600686518140cbedc
Harjot <hajotsingh2552@gmail.com>
2025-04-09 20:43:08 -0600

    Creating the auto scroll function whenever the new message is coming

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import { useChatStore } from "../store/useChatStore";
@@ -9,12 +9,17 @@ import { formatMessageTime } from "../lib/utils";
 const ChatContainer = () => {
   const { isMessagesLoading, messages, getMessages, selectedUser, subscribeToMessages, unsubscribeToMessages }  = useChatStore();
   const { authUser } =   useAuthStore();
+  const messageEndRef = useRef(null)
 
   useEffect(() => {
     getMessages(selectedUser._id);
     subscribeToMessages();
     return () => unsubscribeToMessages();
-  }, [selectedUser._id, getMessages]);
+  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeToMessages]);
+
+  useEffect(()=>{
+    messageEndRef.current.scrollIntoView({behavior: "smooth"})
+  },[])
 
   if (isMessagesLoading)
     return (
@@ -33,6 +38,7 @@ const ChatContainer = () => {
           <div
             key={message._id}
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}
           >
             <div className=" chat-image avatar">
               <div className="size-10 rounded-full border">
