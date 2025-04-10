8a3bc73a5d7dbf6dde405675e225357596d8a15f
Harjot <hajotsingh2552@gmail.com>
2025-04-09 21:09:01 -0600

    Filtering the users who are online and offline

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -14,6 +14,8 @@ useChatStore();
     getUsers();
   }, [getUsers]);
 
+  const filteredUsers = showOnlineOnly ? users.filter(user => onlineUsers.includes(user._id)) : users; 
+
   if (isUsersLoading) return <SidebarSkeleton />;
   return (
     <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
