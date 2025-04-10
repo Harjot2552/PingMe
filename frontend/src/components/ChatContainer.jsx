5831a353b56533a61b7371d0076b5d3ac0ee21ef
Harjot <hajotsingh2552@gmail.com>
2025-04-09 18:57:35 -0600

    Fixed some error in chatStore

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -25,11 +25,15 @@ const ChatContainer = () => {
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
-        <div className="flex-1 overflow-y-auto p-4 space-y-4">
-          {messages.map((message)=>{
-            <div key={message._id} className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
-              <div className="chat-image avatar">
-                <div className="size-10 rounded-full border">
+   
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+          >
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
                 <img
                   src={
                     message.senderId === authUser._id
@@ -38,15 +42,15 @@ const ChatContainer = () => {
                   }
                   alt="profile pic"
                 />
-                </div>
-
               </div>
-                  <div className="chat-header mb-1">
-                    <time className="text-xs opacity-50 ml-1">{formatMessageTime(message.createdAt)}</time>
-
-                  </div>
-                  <div className="chat-bubble flex">
-                  {message.image && (
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
                 <img
                   src={message.image}
                   alt="Attachment"
@@ -54,10 +58,10 @@ const ChatContainer = () => {
                 />
               )}
               {message.text && <p>{message.text}</p>}
-                  </div>
             </div>
-          })}
-        </div>
+          </div>
+        ))}
+      </div>
       <MessageInput />
     </div>
   );
