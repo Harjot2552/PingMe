d525b0a2a5af5c5ad14b9aa938ecb6e26c9563ae
Harjot <hajotsingh2552@gmail.com>
2025-04-09 18:17:28 -0600

    Showing the time of the message when user send and received the message

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -28,11 +28,21 @@ const ChatContainer = () => {
             <div key={message._id} className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
               <div className="chat-image avatar">
                 <div className="size-10 rounded-full border">
-
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
                 </div>
 
               </div>
+                  <div className="chat-header mb-1">
+                    <time className="text-xs opacity-50 ml-1">{message.createdAt}</time>
 
+                  </div>
             </div>
           })}
         </div>
